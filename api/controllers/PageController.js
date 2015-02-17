677ac0f1c7c995a514bf6943d02488f64284872a
bivdub <bivdub@gmail.com>
2015-02-17 10:54:48 -0800

    adds env to gitignore

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -4,12 +4,20 @@
  * @description :: Server-side logic for managing pages
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
-var moby = require('moby');
+// var moby = require('moby');
+var twitter = require('twitter');
+var LastFmNode = require('lastfm').LastFmNode;
+
+var lastfm = new LastFmNode({
+  api_key: 'apikey',    // sign-up for a key at http://www.last.fm/api
+  secret: 'secret',
+  useragent: 'appname/vX.X MyApp' // optional. defaults to lastfm-node.
+});
 
 module.exports = {
   index: function(req,res){
-      console.log(moby.search('furious'));
-      console.log(moby.reverseSearch('furious'));
+      // console.log(moby.search('furious'));
+      // console.log(moby.reverseSearch('furious'));
       res.view('index');
     }
 };
