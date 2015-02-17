c18b54690af46f98559a3c462aeec6e3bcda3981
bivdub <bivdub@gmail.com>
2015-02-17 10:58:02 -0800

    testing gitignore

--- config/env/development.js
+++ config/env/development.js
@@ -12,6 +12,9 @@
 
 module.exports = {
 
+
+  TESTTESTTEST
+
   /***************************************************************************
    * Set the default database connection for models in the development       *
    * environment (see config/connections.js and config/models.js )           *
